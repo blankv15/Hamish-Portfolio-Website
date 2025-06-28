f09897a2064b5efee72912616e02ad3c7c3c639e
HamishMac <hchhagan@gmail.com>
2025-06-28 21:42:46 +1200

    working on card, gonna try out glass

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -1,37 +1,28 @@
-import './DisplayCard.css'
+import "./DisplayCard.css";
 
- {/*.  image size is 300px* 160px */}
+{
+  /*.  image size is 300px* 160px */
+}
 function DisplayCard() {
-
   return (
     <>
-
       <div className="card">
-        <ol>
-          <li>
-            <img src="https://picsum.photos/300/160" alt="project image" />
-          </li>
-          <li>
-            <h4>Norway Fjord Adventures</h4>
-          </li>
-          <li>
-            <span>With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on 
-              and around the fjords of Norway</span>
-          </li>
-            <li a='button-li'>
-            <button>Read More</button> 
-         </li>
-          
-          
-        </ol>
+        <img src="https://picsum.photos/300/160" alt="project image" />
 
+        <div id="text-infomation">
+          <h4>Norway Fjord Adventures</h4>
+
+          <p>
+            With Fjord Tours you can explore more of the magical fjord
+            landscapes with tours and activities on and around the fjords of
+            Norway
+          </p>
+
+          <button>Read More</button>
+        </div>
       </div>
-
-
-
-
     </>
-  )
+  );
 }
 
-export default DisplayCard
+export default DisplayCard;
